e262998375cc3cf40de28d1c80e781e9a56f9773
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-04-02 11:44:32 +0200

    fixed naming

--- output_executables.py
+++ output_executables.py
@@ -7,10 +7,13 @@ def print_directories():
 def print_executables():
     for directory in directories:
         print(directory)
+    try:
         files = os.listdir(directory)
         executables = [file for file in files if os.access(os.path.join(directory, file), os.X_OK)]
         for executable in executables:
             print(executable)
+    except FileNotFoundError:
+        print(f"Directory {directory} not found")
 
 if __name__ == "__main__":
     print_directories()
