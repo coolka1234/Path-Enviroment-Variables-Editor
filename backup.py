ce843ae594c68664e8b976631a8db06d6b3ac6f4
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-04-04 21:00:25 +0200

    working on backup function

--- backup.py
+++ backup.py
@@ -2,9 +2,14 @@ import os
 import subprocess
 import backup_script_functions as bsf
 import datetime
-def backup_catalog(pathOfFileToBackup, newBackupPath=None, nameOfBackup=datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+"backup.zip"):
-    if newBackupPath is not None and not os.path.exists(newBackupPath):
-        bsf.createEnviromentalVariable(name="BACKUPS_DIR", value=newBackupPath)
+import sys
+def backup_catalog(pathOfFileToBackup, newBackupPath=None, nameOfBackup=datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")+"backup.zip"):
+    if newBackupPath is not None:
+        bsf.createEnviromentalVariable(name="BACKUPS_DIR", value=None)
     else :
-        bsf.createEnviromentalVariable(value=newBackupPath)
-    subprocess.run(["powershell", "Compress-Archive", "-Path", ".\\empty_folder", newBackupPath, nameOfBackup], check=True)
+        bsf.createEnviromentalVariable(value=None)
+    subprocess.run(["powershell", "Compress-Archive", ".\\empty_folder", os.environ["BACKUPS_DIR"] + nameOfBackup], check=True)
+    bsf.copyFiles(pathOfFileToBackup, os.environ["BACKUPS_DIR"]+ nameOfBackup, nameOfBackup)
+
+if __name__ == "__main__":
+    backup_catalog(pathOfFileToBackup=os.path.normpath(sys.argv[1]), newBackupPath=".\\backups")
--- backup_script_functions.py
+++ backup_script_functions.py
@@ -1,19 +1,31 @@
 import os
 import subprocess
+import datetime
+import time
 def createEnviromentalVariable(name="BACKUPS_DIR", value=None):
     if value is None:
-        value = os.path.expanduser(r"~\\backups")
+        value = os.path.expanduser(r"~")
+        value+=r"\backups"
     os.environ[name] = value
     print(f"Environmental variable {name} set to {value}")
 
-def copyFiles(source, destination):
+def copyFiles(source, destination, backupName):
     subprocess.run(["robocopy", source, destination, "/E"])
+    renameFiles(destination, datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")+"-"+backupName)
     print(f"Files copied from {source} to {destination}")
 
 def deleteFiles(path):
     subprocess.run(["rmdir", path, "/s", "/q"])
     print(f"Files deleted from {path}")
-
+def renameFiles(path, backupName):
+    for file in os.listdir(path):
+        time_of_file = os.path.getmtime(os.path.join(path, file))
+        time_of_file_str = time.strftime('%Y_%m_%d_%H_%M_%S', time.gmtime(time_of_file))
+        file_name, file_extension = os.path.splitext(file)
+        try:
+            os.rename(os.path.join(path, file), os.path.join(time_of_file_str,backupName, file_extension))
+        except FileNotFoundError:
+            print(f"File {file} from {path} not found")
 def backup_catalog(pathOfFileToBackup, newBackupPath=None):
     if newBackupPath is not None:
         createEnviromentalVariable(name="BACKUPS_DIR", value=newBackupPath)
