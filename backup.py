5c939566d4ad7d68c2e6469640f1506bbe7d510d
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-04-06 13:15:11 +0200

    changed logs from json to csv

--- backup.py
+++ backup.py
@@ -3,9 +3,10 @@ import subprocess
 import backup_script_functions as bsf
 import datetime
 import sys
-def backup_catalog(pathOfFileToBackup, newBackupPath=None, nameOfBackup=datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")+"backup.zip"):
+import create_backup_log as cbl
+def backup_catalog(pathOfFileToBackup, newBackupPath=None, nameOfBackup=datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")+"backup.zip"):
     if newBackupPath is not None:
-        bsf.createEnviromentalVariable(name="BACKUPS_DIR", value=None)
+        bsf.createEnviromentalVariable(name="BACKUPS_DIR", value=newBackupPath)
     else :
         bsf.createEnviromentalVariable(value=None)
     if not os.path.exists(os.environ["BACKUPS_DIR"]):
@@ -13,6 +14,7 @@ def backup_catalog(pathOfFileToBackup, newBackupPath=None, nameOfBackup=datetime
     subprocess.run(["powershell","mkdir", (nameOfBackup)])
     bsf.copyFiles(pathOfFileToBackup, nameOfBackup, nameOfBackup)
     bsf.renameFiles(nameOfBackup, nameOfBackup)
+    cbl.create_backup_log(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"),os.path.join(os.environ["BACKUPS_DIR"], nameOfBackup), nameOfBackup)
     subprocess.run(["powershell", "Compress-Archive", nameOfBackup,os.path.join(os.environ["BACKUPS_DIR"], nameOfBackup), "-Force"], check=True)
     bsf.deleteFolder(nameOfBackup)
 
--- create_backup_log.py
+++ create_backup_log.py
@@ -1,9 +1,12 @@
 import os
-import json
+import csv
 def create_backup_log(dateOfBackup, backupPath, fileName):
-    file_path = os.path.join(os.environ["BACKUPS_DIR"],"/backup_log.json")
-    with open(file_path, 'a') as file:
-        json.dump(create_log_dict(dateOfBackup, backupPath, fileName), file)
+    print(os.environ["BACKUPS_DIR"])
+    file_path = os.path.join(os.environ["BACKUPS_DIR"],r"backup_log.csv")
+    print(file_path)
+    with open(file_path, 'a', newline='') as file:
+        writer= csv.writer(file)
+        writer.writerow([dateOfBackup, backupPath, fileName])
         file.close()
 def create_log_dict(dateOfBackup, backupPath, fileName):
     log_dict = {
